d835283d8c5377d0ffe40d0764dbe3c9c88a45cf
Evgeny Drobotun <evgeny@drobotun.su>
2026-02-21 19:35:07 +0300

    Update bigint.h

--- source/bigint.h
+++ source/bigint.h
@@ -1,9 +1,9 @@
 /****************************************************************************
 **
-** Copyright (C) 2025 Evgeny Drobotun, Anna Boykova
+** Copyright (C) 2026 Evgeny Drobotun, Anna Boykova.
 ** Contact: evgeny@drobotun.su
 **
-** This file (along with the "bigint.cpp" file) is module that implements
+** This file (along with the "BigInt.cpp" file) is module that implements
 ** arithmetic and logical operation on long numbers.
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a
@@ -46,17 +46,11 @@ enum class ExceptionCode {
     InvalidValue
 };
 
-class BigIntException: public std::exception
-{
-public:
-    BigIntException(const std::string &message);
-    BigIntException(const ExceptionCode code);
-    BigIntException(const std::string &message, const ExceptionCode code);
-    const char* what() const noexcept override;
-    ExceptionCode getExceptionCode() const noexcept;
-private:
-    std::string m_message{""};
-    ExceptionCode m_code{ExceptionCode::Unknown};
+enum class Radix {
+    Hex,
+    Dec,
+    Oct,
+    Bin
 };
 
 enum class MulMethod {
@@ -64,80 +58,96 @@ enum class MulMethod {
     FastFourierTransform
 };
 
-using digit_t = unsigned char;
+class BigIntException: public std::exception
+{
+public:
+    BigIntException(const std::string& message);
+    BigIntException(const ExceptionCode code);
+    BigIntException(const std::string& message, const ExceptionCode code);
+    const char* what() const noexcept override;
+    ExceptionCode getExceptionCode() const noexcept;
+private:
+    std::string m_message{""};
+    ExceptionCode m_code{ExceptionCode::Unknown};
+};
 
-const double PI = std::acos(-1);
-const size_t LLONG_SIZE = 18;
+const double PI{std::acos(-1)};
+const size_t LLONG_SIZE{8};
 
 class BigInt
 {
 private:
-    std::vector<digit_t> m_digits;
+    std::vector<uint8_t> m_bytes;
     bool m_sign = false;
+    MulMethod m_mulMethod{MulMethod::Karatsuba};
 public:
     BigInt();
-    BigInt(const std::string &strValue, const size_t radix = 10);
-    BigInt(const char *strValue, const size_t radix = 10);
+    BigInt(const std::string& value, const Radix radix = Radix::Dec);
+    BigInt(const char* strValue, const Radix radix = Radix::Dec);
     BigInt(const long long value);
     BigInt(const int value);
     BigInt(const size_t value);
-    BigInt(const BigInt &other);
+    BigInt(const BigInt& other);
 #if __cplusplus >= 201103L
-    BigInt(BigInt &&other);
+    BigInt(BigInt&& other) noexcept;
 #endif
     ~BigInt();
     BigInt& operator = (const long long value);
-    BigInt& operator = (const BigInt &other);
+    BigInt& operator = (const BigInt& other);
 #if __cplusplus >= 201103L
-    BigInt& operator = (BigInt &&other);
+    BigInt& operator = (BigInt&& other)noexcept;
 #endif
 
-    size_t size() const;
+    size_t length() const;
     bool isEven() const;
-    unsigned long long numBits();
-    std::string toString(const size_t radix = 10, const bool upper = false) const;
+    size_t numBits();
+    std::string toString(const Radix radix = Radix::Dec, const bool upper = false) const;
+    std::string toHex(const bool upper = false);
+    std::string toDec();
+    std::string toOct();
+    std::string toBin();
     int toInt() const;
     long toLong() const;
     long long toLongLong() const;
+    unsigned int toUInt() const;
     unsigned long toULong() const;
     unsigned long long toULongLong() const;
-
     void setMulMethod(MulMethod mul_mwethod);
 
-    bool operator == (const BigInt &other) const;
+    bool operator == (const BigInt& other) const;
     bool operator == (const long long other) const;
-    bool operator != (const BigInt &other) const;
+    bool operator != (const BigInt& other) const;
     bool operator != (const long long other) const;
-    bool operator > (const BigInt &other) const;
+    bool operator > (const BigInt& other) const;
     bool operator > (const long long other) const;
-    bool operator < (const BigInt &other) const;
+    bool operator < (const BigInt& other) const;
     bool operator < (const long long other) const;
-    bool operator >= (const BigInt &other) const;
+    bool operator >= (const BigInt& other) const;
     bool operator >= (const long long other) const;
-    bool operator <= (const BigInt &other) const;
+    bool operator <= (const BigInt& other) const;
     bool operator <= (const long long other) const;
 
-    BigInt operator << (const unsigned long long shift) const;
-    BigInt operator >> (const unsigned long long shift) const;
+    BigInt operator << (const size_t shift) const;
+    BigInt operator >> (const size_t shift) const;
 
-    BigInt operator + (const BigInt &other) const;
+    BigInt operator + (const BigInt& other) const;
     BigInt operator + (const long long other) const;
-    BigInt operator - (const BigInt &other) const;
+    BigInt operator - (const BigInt& other) const;
     BigInt operator - (const long long other) const;
-    BigInt operator * (const BigInt &other) const;
+    BigInt operator * (const BigInt& other) const;
     BigInt operator * (const long long other) const;
-    BigInt operator / (const BigInt &other) const;
+    BigInt operator / (const BigInt& other) const;
     BigInt operator / (const long long other) const;
-    BigInt operator % (const BigInt &other) const;
+    BigInt operator % (const BigInt& other) const;
     BigInt operator % (const long long other) const;
 
-    BigInt operator & (const BigInt &other) const;
-    BigInt operator & (const long long other) const;
-    BigInt operator | (const BigInt &other) const;
+    BigInt operator&  (const BigInt& other) const;
+    BigInt operator&  (const long long other) const;
+    BigInt operator | (const BigInt& other) const;
     BigInt operator | (const long long other) const;
-    BigInt operator ^ (const BigInt &other) const;
+    BigInt operator ^ (const BigInt& other) const;
     BigInt operator ^ (const long long other) const;
-    BigInt operator ~ () const &;
+    BigInt operator ~ () const& ;
 
     BigInt operator -- ();
     BigInt operator ++ ();
@@ -148,117 +158,113 @@ public:
     BigInt& operator <<= (const size_t shift);
     BigInt& operator >>= (const size_t shift);
 
-    BigInt& operator += (const BigInt &other);
+    BigInt& operator += (const BigInt& other);
     BigInt& operator += (const long long other);
-    BigInt& operator -= (const BigInt &other);
+    BigInt& operator -= (const BigInt& other);
     BigInt& operator -= (const long long other);
-    BigInt& operator *= (const BigInt &other);
+    BigInt& operator *= (const BigInt& other);
     BigInt& operator *= (const long long other);
-    BigInt& operator /= (const BigInt &other);
+    BigInt& operator /= (const BigInt& other);
     BigInt& operator /= (const long long other);
-    BigInt& operator %= (const BigInt &other);
+    BigInt& operator %= (const BigInt& other);
     BigInt& operator %= (const long long other);
-    BigInt& operator |= (const BigInt &other);
+    BigInt& operator |= (const BigInt& other);
     BigInt& operator |= (const long long other);
-    BigInt& operator &= (const BigInt &other);
+    BigInt& operator &= (const BigInt& other);
     BigInt& operator &= (const long long other);
-    BigInt& operator ^= (const BigInt &other);
+    BigInt& operator ^= (const BigInt& other);
     BigInt& operator ^= (const long long other);
 
-    BigInt operator -() const &;
-    BigInt operator +() const &;
+    BigInt operator -() const& ;
+    BigInt operator +() const& ;
 
-    friend BigInt strToBigInt(const std::string &value, const size_t radix);
+    friend std::ostream& operator << (std::ostream& out, const BigInt& value);
+    friend std::istream& operator >> (std::istream& in, BigInt& value);
 
-    friend std::ostream& operator << (std::ostream &out, const BigInt &value);
-    friend std::istream& operator >> (std::istream &in, BigInt &value);
+    friend BigInt shr(const BigInt& value, const size_t shift);
+    friend BigInt shl(const BigInt& value, const size_t shift);
 
-    friend BigInt shr(const BigInt &value, const size_t shift);
-    friend BigInt shl(const BigInt &value, const size_t shift);
+    friend BigInt sqr(const BigInt& value);
+    friend BigInt pow(const BigInt& value, const long long exp);
+    friend BigInt pow(const BigInt& value, const BigInt exp);
 
-    friend BigInt sqr(const BigInt &value);
-    friend BigInt pow(const BigInt &value, const long long exp);
-    friend BigInt pow(const BigInt &value, const BigInt exp);
+    friend BigInt powMod(const BigInt& value, const long long exp, const BigInt& mod);
+    friend BigInt powMod(const BigInt& value, const BigInt exp, const BigInt& mod);
 
-    friend BigInt powMod(const BigInt &value, const long long exp, const BigInt &mod);
-    friend BigInt powMod(const BigInt &value, const BigInt exp, const BigInt &mod);
+    friend BigInt sqrt(const BigInt& value);
+    friend BigInt root(const BigInt& value, const long long exp);
+    friend BigInt root(const BigInt& value, const BigInt& exp);
 
-    friend BigInt sqrt(const BigInt &value);
-    friend BigInt root(const BigInt &value, const long long exp);
-    friend BigInt root(const BigInt &value, const BigInt &exp);
+    friend BigInt abs(const BigInt& value);
 
-    friend BigInt abs(const BigInt &value);
+    friend BigInt gcd(const BigInt& value_1, const BigInt& value_2);
+    friend BigInt gcdExt(const BigInt& value_1, const BigInt& value_2,
+                          BigInt& factor_1, BigInt& factor_2);
 
-    friend BigInt gcd(const BigInt &value_1, const BigInt &value_2);
-    friend BigInt gcdExt(const BigInt &value_1, const BigInt &value_2,
-                          BigInt &factor_1, BigInt &factor_2);
-
-    friend int getLegendreSymbol(const BigInt &value_a, const BigInt &value_p);
-    friend int getJacobiSymbol(const BigInt &value_a, const BigInt &value_p);
+    friend int getLegendreSymbol(const BigInt& value_a, const BigInt& value_p);
+    friend int getJacobiSymbol(const BigInt& value_a, const BigInt& value_p);
 
 
 private:
     BigInt shlBitsBase(const size_t shift) const;
     BigInt shrBitsBase(const size_t shift) const;
 
-    BigInt shlDigitsBase(const size_t shift) const;
-    BigInt shrDigitsBase(const size_t shift) const;
+    BigInt shlBytesBase(const size_t shift) const;
+    BigInt shrBytesBase(const size_t shift) const;
 
-    BigInt orBase(const BigInt &value) const;
-    BigInt andBase(const BigInt &value) const;
-    BigInt xorBase(const BigInt &value) const;
+    BigInt orBase(const BigInt& value) const;
+    BigInt andBase(const BigInt& value) const;
+    BigInt xorBase(const BigInt& value) const;
     BigInt notBase() const;
 
-    BigInt addBase(const BigInt &value) const;
-    BigInt subBase(const BigInt &value) const;
-    BigInt mulNative(const BigInt &value) const;
-    BigInt mulKaratsuba(const BigInt &value) const;
-    BigInt mulFft(const BigInt &value) const;
-    BigInt mulBase(const BigInt &value) const;
-    BigInt divNative(const BigInt &value) const;
-    BigInt divNewtonRaphson(const BigInt &value) const;
-    BigInt divBase(const BigInt &value) const;
-    BigInt remBase(const BigInt &value) const;
+    BigInt addBase(const BigInt& value) const;
+    BigInt subBase(const BigInt& value) const;
+    BigInt mulNative(const BigInt& value) const;
+    BigInt mulKaratsuba(const BigInt& value) const;
+    BigInt mulFft(const BigInt& value) const;
+    BigInt mulBase(const BigInt& value) const;
+    BigInt divNative(const BigInt& value) const;
+    BigInt divNewtonRaphson(const BigInt& value) const;
+    BigInt divBase(const BigInt& value) const;
+    BigInt remBase(const BigInt& value) const;
 
     BigInt powBase(long long exp) const;
-    BigInt powBase(const BigInt &exp) const;
-    BigInt powModBase(const long long exp, const BigInt &mod) const;
-    BigInt powModBase(const BigInt &exp, const BigInt &mod) const;
+    BigInt powBase(const BigInt& exp) const;
+    BigInt powModBase(const long long exp, const BigInt& mod) const;
+    BigInt powModBase(const BigInt& exp, const BigInt& mod) const;
     BigInt rootBase(const long long exp) const;
-    BigInt rootBase(const BigInt &exp) const;
+    BigInt rootBase(const BigInt& exp) const;
 
-    void normalize();
-
-    MulMethod m_mulMethod{MulMethod::Karatsuba};
+    void normalise();
 };
 
-BigInt strToBigInt(const std::string &strValue, const size_t radix = 10);
-BigInt strToBigInt(const char *strValue, size_t radix = 10);
+BigInt strToBigInt(const std::string& strValue, const Radix radix = Radix::Dec);
+BigInt strToBigInt(const char *strValue, const Radix radix = Radix::Dec);
 
-std::ostream& operator << (std::ostream &out, const BigInt &value);
-std::istream& operator >> (std::istream &in, BigInt &value);
+std::ostream& operator << (std::ostream& out, const BigInt& value);
+std::istream& operator >> (std::istream& in, BigInt& value);
 
-BigInt shr(const BigInt &value, const size_t shift);
-BigInt shl(const BigInt &value, const size_t shift);
+BigInt shr(const BigInt& value, const size_t shift);
+BigInt shl(const BigInt& value, const size_t shift);
 
-BigInt sqr(const BigInt &value);
-BigInt pow(const BigInt &value, const long long exp);
-BigInt pow(const BigInt &value, const BigInt exp);
+BigInt sqr(const BigInt& value);
+BigInt pow(const BigInt& value, const long long exp);
+BigInt pow(const BigInt& value, const BigInt exp);
 
-BigInt powMod(const BigInt &value, const long long exp, const BigInt &mod);
-BigInt powMod(const BigInt &value, const BigInt exp, const BigInt &mod);
+BigInt powMod(const BigInt& value, const long long exp, const BigInt& mod);
+BigInt powMod(const BigInt& value, const BigInt exp, const BigInt& mod);
 
-BigInt sqrt(const BigInt &value);
-BigInt root(const BigInt &value, const long long exp);
-BigInt root(const BigInt &value, const BigInt &exp);
+BigInt sqrt(const BigInt& value);
+BigInt root(const BigInt& value, const long long exp);
+BigInt root(const BigInt& value, const BigInt& exp);
 
-BigInt abs(const BigInt &value);
+BigInt abs(const BigInt& value);
 
-BigInt gcd(const BigInt &value_1, const BigInt &value_2);
-BigInt gcdExt(const BigInt &value_1, const BigInt &value_2,
-               BigInt &factor_1, BigInt &factor_2);
+BigInt gcd(const BigInt& value_1, const BigInt& value_2);
+BigInt gcdExt(const BigInt& value_1, const BigInt& value_2,
+               BigInt& factor_1, BigInt& factor_2);
 
-int getLegendreSymbol(const BigInt &value_a, const BigInt &value_p);
-int getJacobiSymbol(const BigInt &value_a, const BigInt &value_p);
+int getLegendreSymbol(const BigInt& value_a, const BigInt& value_p);
+int getJacobiSymbol(const BigInt& value_a, const BigInt& value_p);
 
 #endif // BIGINT_H
